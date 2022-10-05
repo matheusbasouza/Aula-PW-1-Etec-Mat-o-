d6aec5b935ada3ff4569e31a20f253d9ea37b420
Aluno <aluno@etec.sp.gov.br>
2022-10-05 20:38:37 -0300

    Aula 1 parte 2: Funções básicas e matemáticas de JavaScript

---
+++ Aula 2/src/index.js
@@ -0,0 +1,26 @@
+function calculaNumeros(n1, n2, op){
+    if(op == "+"){
+        return (n1 + n2);
+    } 
+    else if (op == "-") {
+        return (n1 - n2);
+    }
+    else if (op == "/"){
+        return (n1 / n2);
+    }
+    else if (op == "*"){
+        return (n1 * n2);
+    }
+    else if (op == "raiz"){
+        return Math.sqrt(n1 + n2);
+    }
+    else if (op == "potencia"){
+        return Math.pow(n1,n2);
+    }
+    else {
+        return ("Erro!!");
+    }
+
+}
+
+console.log(calculaNumeros(2, 2, "raiz"));
